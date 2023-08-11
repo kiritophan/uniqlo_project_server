3b997f9d6dd030adad72c207f92b84d75a99a855
DaupvDcktech <daupv@dck-tech.com>
2023-08-11 17:48:38 +0700

    update carts

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,16 +1,3 @@
-// /* Create Express Router */
-// import express from 'express'
-// const router = express.Router()
-
-// import v1Api from './v1'
-// router.use('/v1', v1Api)
-
-// import adminMiddleware from '../middlewares/admin.middleware';
-// import adminApi from './admin';
-// router.use('/admin', adminMiddleware.checkAdmin, adminApi);
-
-// export default router;  
-/* Create Express Router */
 import express from 'express'
 const router = express.Router()
 
